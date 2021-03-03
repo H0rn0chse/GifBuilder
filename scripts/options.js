d15be3f5764ad899105fa7158ff2ce7bf1ee0a67
H0rn0chse <h0rn0chse@gmx.de>
2021-03-03 18:59:46 +0100

    Added getter and update on change

--- scripts/options.js
+++ scripts/options.js
@@ -13,6 +13,10 @@ function initPicker () {
 
     colorPicker.color.fromRGBa(255,0,255);
     colorPicker.update();
+
+    colorPicker.on("change", () => {
+        PreviewManager.render();
+    });
 }
 
 export function initOptions () {
@@ -31,5 +35,72 @@ export function initOptions () {
         const blob = PreviewManager.currentBlob;
         exportBlob(blob, "image.gif");
     });
+
+    const inputs = [
+        document.querySelector("#imageWidth"),
+        document.querySelector("#imageHeight"),
+        document.querySelector("#keepDimensions"),
+        document.querySelector("#framesPerSecond"),
+        document.querySelector("#imageQuality"),
+        document.querySelector("#imageDithering"),
+        document.querySelector("#alphaHandling"),
+    ];
+
+    inputs.forEach(elem => {
+        elem.addEventListener("change", evt => {
+            PreviewManager.render();
+        });
+    });
+}
+
+export function setInputDimensions (width, height) {
+    const widthInput = document.querySelector("#imageWidth");
+    const heightInput = document.querySelector("#imageHeight");
+
+    if (widthInput.valueAsNumber === 0) {
+        widthInput.value = width;
+    }
+    if (heightInput.valueAsNumber === 0) {
+        heightInput.value = height;
+    }
+}
+
+export function getWidth () {
+    const input = document.querySelector("#imageWidth");
+    return input.valueAsNumber;
+}
+
+export function getHeight () {
+    const input = document.querySelector("#imageHeight");
+    return input.valueAsNumber;
+}
+
+export function getKeepDimensions () {
+    const checkbox = document.querySelector("#keepDimensions");
+    return checkbox.checked;
+}
+
+export function getFramesPerSecond () {
+    const input = document.querySelector("#framesPerSecond");
+    return input.valueAsNumber;
+}
+
+export function getQuality () {
+    const input = document.querySelector("#imageQuality");
+    return (parseInt(input.max, 10) + 1) - input.valueAsNumber;
+}
+
+export function getDithering () {
+    const select = document.querySelector("#imageDithering");
+    const value = select.value;
+    if (value === "none") {
+        return false;
+    }
+    return value;
+}
+
+export function getAlphaHandling () {
+    const select = document.querySelector("#alphaHandling");
+    return select.value;
 }
 
