14fbbd9babf9fe45fd33d0f66d6a6adee394e5a5
H0rn0chse <h0rn0chse@gmx.de>
2021-03-05 22:57:09 +0100

    Added acknowledgements and licenses

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,12 @@ HintManager.init();
 initImport();
 initOptions();
 
+const acknowledgements = document.querySelector("#acknowledgements");
+acknowledgements.innerHTML = feather.icons["award"].toSvg({ color: "#e2b007" });
+acknowledgements.addEventListener("click", evt => {
+    window.open("./acknowledgements/third-party-licenses.html", '_blank')
+}, { passive: true });
+
 // debugging
 globalThis.HintManager = HintManager;
 globalThis.TimelineManager = TimelineManager;
