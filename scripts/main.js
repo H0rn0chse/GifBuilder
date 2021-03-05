f0d58b753ac89f432f298371800dc7338f2fd151
H0rn0chse <h0rn0chse@gmx.de>
2021-03-05 18:12:23 +0100

    Moved init part

--- scripts/main.js
+++ scripts/main.js
@@ -4,25 +4,23 @@ import { initOptions } from "./options.js";
 import { PreviewManager } from "./PreviewManager.js";
 import { TimelineManager } from "./TimelineManager.js";
 
-export function init () {
-    TimelineManager.init();
-    PreviewManager.init();
-    HintManager.init();
-    initImport();
-    initOptions();
+TimelineManager.init();
+PreviewManager.init();
+HintManager.init();
+initImport();
+initOptions();
 
-    // debugging
-    globalThis.HintManager = HintManager;
-    globalThis.TimelineManager = TimelineManager;
-    globalThis.PreviewManager = PreviewManager;
+// debugging
+globalThis.HintManager = HintManager;
+globalThis.TimelineManager = TimelineManager;
+globalThis.PreviewManager = PreviewManager;
 
-    //======================== Modal picnic.css windows ===============
-    document.addEventListener("keydown", evt => {
-        if (evt.key === "Escape") {
-            const modalWindows = document.querySelectorAll(".modal > [type=checkbox]");
-            modalWindows.forEach(modal => {
-                modal.checked = false
-            });
-        }
-    }, { passive: true });
-}
+//======================== Modal picnic.css windows ===============
+document.addEventListener("keydown", evt => {
+    if (evt.key === "Escape") {
+        const modalWindows = document.querySelectorAll(".modal > [type=checkbox]");
+        modalWindows.forEach(modal => {
+            modal.checked = false
+        });
+    }
+}, { passive: true });
