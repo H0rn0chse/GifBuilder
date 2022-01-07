0fa7f190677d6b192a9c7f3ff367c1973cfe8cb2
H0rn0chse <h0rn0chse@gmx.de>
2022-01-07 21:18:28 +0100

    Updates local dev setup

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect()
-    .use(serveStatic(__dirname))
-    .listen(8080, function(){
-        console.log('Server running on http://localhost:8080');
-    });
+import { startServer } from "@h0rn0chse/socket-server";
+
+startServer({
+    publicPaths: [["/", "/"]]
+});
