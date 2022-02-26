8b70186402859549dc9e0bdcb3a686948c4b6018
H0rn0chse <h0rn0chse@gmx.de>
2022-02-26 21:37:27 +0100

    Updated gh-pages deployment

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import { startServer } from "@h0rn0chse/socket-server";
+import fs from "fs";
+
+const json = fs.readFileSync("./gh-pages-dependencies.json");
+const dependencies = JSON.parse(json);
 
 startServer({
-    publicPaths: [["/", "/"]]
+    publicPaths: dependencies
 });
